80fa0fc8704b3018bc38d931552b92746a214be5
Markus Kainu <markuskainu@gmail.com>
2017-06-29 09:56:28 +0300

    Major changes

--- R/create_an_assignment.R
+++ R/create_an_assignment.R
@@ -4,14 +4,14 @@ create_assignment <- function() {
   library(miniUI)
   library(shiny)
   ui <- miniPage(
-    gadgetTitleBar("Add an assignment"),
+    gadgetTitleBar("Create an assignment"),
     miniContentPanel(
       fillRow(flex = c(7, 3), height = "60px",
               uiOutput("i_lang")),
     fillRow(flex = c(7, 3), height = "60px",
             uiOutput("i_domain")),
       fillRow(flex = c(7, 3), height = "60px",
-              uiOutput("i_question")),
+              uiOutput("i_exercise")),
       htmlOutput("preview")
     )
   )
@@ -19,54 +19,52 @@ create_assignment <- function() {
   server <- function(input, output, session) {
 
     require(dplyr)
-    raw <- pref <- yaml::yaml.load_file(system.file(package = "edu", ... = "data/assignments.yml"))
-    raw_d <- plyr::ldply (raw, data.frame, stringsAsFactors=FALSE)
+    # scan assignments
+    assign_ymls <- list.files(system.file(package = "edu", ... = "data/"),pattern = ".yml", full.names = TRUE)
 
-    langs <- c("en","fi","se","ru")
-    names(langs) <- c("English","Suomi","Svenska","Russian")
+    all_assignments <- data_frame()
+    for (i in assign_ymls){
+      tmp <- yaml::yaml.load_file(i)
+      tmp_d <- plyr::ldply(tmp, data.frame, stringsAsFactors=FALSE)
+      all_assignments <- bind_rows(all_assignments,tmp_d)
+    }
+
+    langs <- gsub("lang_", "", names(all_assignments)[grepl("lang", names(all_assignments))])
 
     output$i_lang <- renderUI({
       radioButtons("o_lang",
-                  label = "language / kieli / språk / язык?",
+                  label = "Select language",
                   choices =  langs,
-                  selected = langs[1],
+                  selected = "en",
                   inline = TRUE,
                   width = "98%")
     })
 
-    translate_title <- data_frame(en = c("Select domain", "Select question"),
-                                  fi = c("Valitse osio", "Valitse kysymys"),
-                                  se = c("Välj domän","Välj fråga"),
-                                  ru = c("Выберите домен","Выберите вопрос"))
-
-
-    translate_domain <- data_frame(en = c("basics","import","export","tidy","transform","visualise","model","communicate","automate"),
-                                   fi = c("perusteet","tuo","vie","siivoa","muokkaa","visualisoi","mallinna","kommunikoi","automatisoi"),
-                                   se = c("grunderna","import","exportera", "snyggt", "omvandla", "visualisera", "modell", "kommunicera", "automatisera"),
-                                   ru = c("основы","импорт","экспорт", "аккуратная", "преобразование", "визуализация", "модель", "общаться", "автоматизировать"))
-
     output$i_domain <- renderUI({
 
-      domains <- translate_domain$en
-      names(domains) <- translate_domain[[input$o_lang]]
+      domains <- unique(all_assignments$domain)
 
-      selectInput("o_domain",
-                label = translate_title[[1, input$o_lang,]],
+      radioButtons("o_domain",
+                label = "Select domain",
                 choices =  domains,
                 selected = domains[1],
+                inline = TRUE,
                 width = "98%")
     })
 
-    output$i_question <- renderUI({
+    output$i_exercise <- renderUI({
 
-      q <- paste0("q_", input$o_lang)
+      lang_ver <- paste0("lang_", input$o_lang)
 
-      ids <- raw_d[raw_d$domain %in% input$o_domain,][["id"]]
-      names(ids) <- raw_d[raw_d$id %in% ids,][[q]]
+      ids <- all_assignments[all_assignments$domain %in% input$o_domain,][["id"]]
+      # Replace all non-translated exercise + language pairs with text "Translation not available"
+      exercises <- all_assignments[all_assignments$id %in% ids,][[lang_ver]]
+      exercises <- ifelse(is.na(exercises), "Translation not available", exercises)
+      names(ids) <- exercises
 
 
-      checkboxGroupInput("o_question",
-                label = translate_title[[2, input$o_lang,]],
+      checkboxGroupInput("o_exercise",
+                label = "Select exercises",
                 choices =  ids,
                 selected = ids[1], width = "98%")
     })
@@ -74,20 +72,27 @@ create_assignment <- function() {
     observeEvent(input$done, {
 
       yamlheader <- paste0("#' ---\n",
-                           "#' title: ", translate_domain[translate_domain$en %in% input$o_domain, input$o_lang],"\n",
+                           "#' title: ", input$o_domain,"\n",
                            "#' author: ",Sys.info()[['user']],"\n",
                            "#' date: '`r Sys.time()`'\n",
-                           "#' ---\n\n")
+                           "#' output:\n",
+                           "#'   html_document:\n",
+                           "#'     theme: united\n",
+                           "#'     toc: true\n",
+                           "#'     toc_float: true\n",
+                           "#'     number_sections: yes\n",
+                           "#'     code_folding: show\n",
+                           "#' ---\n\n",
+                           "#' # Exercises\n\n")
       rstudioapi::insertText(yamlheader)
 
-      ids <- input$o_question
-      ans <- raw_d[raw_d$id %in% ids, "ans"]
-      q <- paste0("q_", input$o_lang)
+      ids <- input$o_exercise
+      lang_ver <- paste0("lang_", input$o_lang)
 
       for (i in 1:length(ids)){
 
         txt <- paste0("\n\n",
-                      "#' *",raw_d[raw_d$id %in% ids[i],][[q]],"*\n",
+                      "#' *",i,". ",all_assignments[all_assignments$id %in% ids[i],][[lang_ver]],"*\n",
                       "#' \n",
                       "#+ ", ids[i], ", eval = FALSE\n",
                       "replace_this_with_your_answer()",
@@ -96,27 +101,25 @@ create_assignment <- function() {
 
       }
 
-      rstudioapi::insertText("\n\n#' ********************************************\n#' # Correct answers \n#' ********************************************\n\n")
+      # Print the correct answers
+      rstudioapi::insertText("\n\n#' ********************************************\n#' # Correct answers\n\n")
 
       for (i in 1:length(ids)){
 
         txt <- paste0("\n\n",
-                      "#' *",raw_d[raw_d$id %in% ids[i],][[q]],"*\n",
+                      "#' *",i,". ",all_assignments[all_assignments$id %in% ids[i],][[lang_ver]],"*\n",
                       "#' \n",
                       "#+ ", ids[i], "_answer, eval = FALSE\n",
-                      ans[i],
+                      all_assignments[all_assignments$id %in% ids[i], "ans"],
                       "\n\n\n")
         rstudioapi::insertText(txt)
 
       }
 
-
-
-
         stopApp()
       })
 
   }
-  viewer <- dialogViewer("Add an assignment", width = 1200, height = 1000)
+  viewer <- dialogViewer("Create an assignment", width = 1200, height = 1000)
   runGadget(ui, server, viewer = viewer)
   }
